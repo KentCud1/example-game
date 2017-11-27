1e227cf0ac4dfca5a376d8fdcf27aa9b948f2160
KentCud1 <kentcudi@gmail.com>
2017-11-27 01:08:31 -0500

    new script

---
+++ New Unity Project/Assets/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+
+    /// <summary>
+    /// Testing 1,2... Testing
+    /// </summary>
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
